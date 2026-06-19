27d62b25f05c40cd709104a6ec93ea877e3f7ff1
John Lewis <github@jlewis.sh>
2026-06-19 12:28:47 -0500

    fonts: add font_family to AppConfig

--- crates/carbo/src/app/state/config.rs
+++ crates/carbo/src/app/state/config.rs
@@ -1,7 +1,13 @@
 pub struct AppConfig {
-  pub font_size: f32,
+  pub font_size:   f32,
+  pub font_family: Option<String>,
 }
 
 impl Default for AppConfig {
-  fn default() -> Self { Self { font_size: 16.0 } }
+  fn default() -> Self {
+    Self {
+      font_size:   16.0,
+      font_family: None,
+    }
+  }
 }
