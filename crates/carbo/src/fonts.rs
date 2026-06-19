f60fc2efcb77db2a449c990928ac55ce97c18e3a
John Lewis <github@jlewis.sh>
2026-06-19 12:59:32 -0500

    fonts: add method `resolve_face`

--- crates/carbo/src/fonts.rs
+++ crates/carbo/src/fonts.rs
@@ -1,7 +1,11 @@
-use std::{fmt, sync::Mutex};
+use std::{
+  fmt,
+  sync::{Mutex, MutexGuard},
+};
 
 use fontique::{
-  Collection, CollectionOptions, SourceCache, SourceCacheOptions,
+  Attributes, Collection, CollectionOptions, GenericFamily, QueryFamily,
+  QueryFont, QueryStatus, SourceCache, SourceCacheOptions,
 };
 
 pub struct FontContext {
@@ -33,5 +37,46 @@ impl FontContext {
       collection:   Mutex::new(collection),
       source_cache: Mutex::new(source_cache),
     }
+  }
+
+  fn get_mut(
+    &self,
+  ) -> (MutexGuard<'_, Collection>, MutexGuard<'_, SourceCache>) {
+    (
+      self
+        .collection
+        .lock()
+        .expect("font collection mutex poisoned"),
+      self
+        .source_cache
+        .lock()
+        .expect("font source cache mutex poisoned"),
+    )
+  }
+
+  pub fn resolve_face(
+    &self,
+    family: Option<&str>,
+    attrs: Attributes,
+  ) -> Option<QueryFont> {
+    let (mut collection, mut cache) = self.get_mut();
+
+    // build query
+    let mut query = collection.query(&mut cache);
+    let mut family_desc = vec![QueryFamily::Generic(GenericFamily::Monospace)];
+    if let Some(family) = family {
+      family_desc.insert(0, QueryFamily::Named(family));
+    }
+    query.set_families(family_desc);
+    query.set_attributes(attrs);
+
+    // search for families
+    let mut found = None;
+    query.matches_with(|qf| {
+      found = Some(qf.clone());
+      QueryStatus::Stop
+    });
+
+    found
   }
 }
