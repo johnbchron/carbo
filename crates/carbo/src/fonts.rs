36b44e16d5caf78c7f523277aa0fb7f31fcb1df2
John Lewis <github@jlewis.sh>
2026-06-20 14:40:19 -0500

    fonts: implement resolve_terminal_font

--- crates/carbo/src/fonts.rs
+++ crates/carbo/src/fonts.rs
@@ -4,9 +4,11 @@ use std::{
 };
 
 use fontique::{
-  Attributes, Collection, CollectionOptions, GenericFamily, QueryFamily,
-  QueryFont, QueryStatus, SourceCache, SourceCacheOptions,
+  Attributes, Collection, CollectionOptions, FontStyle, FontWeight, FontWidth,
+  GenericFamily, QueryFamily, QueryFont, QueryStatus, SourceCache,
+  SourceCacheOptions,
 };
+use skrifa::{FontRef, MetadataProvider};
 use tracing::instrument;
 use vello::peniko::linebender_resource_handle::FontData;
 
@@ -20,14 +22,9 @@ pub struct FontContext {
 
 #[derive(Debug)]
 pub struct TerminalFonts {
-  pub family_name: String,
-  pub regular:     FontHandle,
-  pub metrics:     CellMetrics,
-}
-
-#[derive(Debug)]
-pub struct FontHandle {
-  data: FontData,
+  pub family_name:  String,
+  pub regular:      FontData,
+  pub cell_metrics: CellMetrics,
 }
 
 /// All lengths in logical px.
@@ -111,6 +108,63 @@ impl FontContext {
     });
 
     found
+  }
+
+  #[instrument(skip_all)]
+  pub fn resolve_terminal_fonts(
+    &self,
+    family: Option<&str>,
+    size_pt: f32,
+  ) -> TerminalFonts {
+    let regular_attrs =
+      Attributes::new(FontWidth::NORMAL, FontStyle::Normal, FontWeight::NORMAL);
+    let regular = self
+      .resolve_face(family, regular_attrs)
+      .expect("failed to resolve a regular font");
+
+    let family_name = self
+      .get_lock()
+      .0
+      .family_name(regular.family.0)
+      .expect("could not find family name")
+      .to_owned();
+
+    let font_data = FontData {
+      data:  regular.blob.clone(),
+      index: regular.index,
+    };
+
+    let font_ref =
+      FontRef::from_index(font_data.data.as_ref(), font_data.index)
+        .expect("failed to read font data");
+
+    // use default axis location
+    let axis_collection = font_ref.axes();
+    let location = axis_collection.location::<&[(&str, f32)]>(&[]);
+
+    // a point is 1/72 of an inch, and standard DPI is 96 PPI.
+    let size = skrifa::instance::Size::new(size_pt / 72.0 * 96.0);
+
+    let metrics = font_ref.metrics(size, &location);
+    let glyph_metrics = font_ref.glyph_metrics(size, &location);
+    let rep_glyph_id = font_ref
+      .charmap()
+      .map('M')
+      .expect("font does not contain M glyph");
+    let advance = glyph_metrics.advance_width(rep_glyph_id).unwrap();
+
+    let cell_metrics = CellMetrics {
+      advance,
+      ascent: metrics.ascent,
+      descent: metrics.descent,
+      line_gap: metrics.leading,
+    };
+
+    TerminalFonts {
+      family_name,
+      regular: font_data,
+      cell_metrics,
+    }
   }
 
   #[instrument(skip_all)]
